01781754c7a02d6065a4230cabe4e6eadef05be9
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-10 16:30:43 -0300

    add(-): added form to film page

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,6 +1,23 @@
+async function fetchFilms() {
+    const res = await fetch('https://swapi.dev/api/films');
+    const data = await res.json();
+    console.log(data)
+    return data.results;
+}
 
-export default function FilmsPage(){
+
+export default async function FilmsPage(){
+    const films = await fetchFilms();
     return (
-        <div> Film Page</div>
+        <ul> 
+            {films.map(film =>(
+                <li key={film.episodeid} className="bg-slate-400 mb-2 p-4 rounded-md">
+                    <div>
+                        <h5>{film.title}</h5>
+                        <h5>{film.episode_id}</h5>
+                    </div>
+                </li>
+            ))}
+        </ul>
     );
 }
