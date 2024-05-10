b2d44adc211d8d68c2ad9fbb94e0ecb8f79b1b8c
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-10 17:05:38 -0300

    add(-): added style mods

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function fetchFilms() {
     const res = await fetch('https://swapi.dev/api/films');
     const data = await res.json();
@@ -11,11 +13,14 @@ export default async function FilmsPage(){
     return (
         <ul> 
             {films.map(film =>(
-                <li key={film.episodeid} className="bg-slate-400 mb-2 p-4 rounded-md">
-                    <div>
-                        <h5>{film.title}</h5>
-                        <h5>{film.episode_id}</h5>
-                    </div>
+                <li key={film.episodeid} className="bg-slate-900 mb-2 p-2">
+                    <article className="flex items-start spacex-6 p-6">
+                        <Image src={film.image} alt="" width={"60"} height={88} className="flex-none rounded-md bg-slate-100"/>
+                        <div className="min-w-0 relative flex-auto pr-20 pl-3">
+                            <h2 className="font-semibold text-salte-900 truncate pr-20">{film.title}</h2>
+                            <h5 className="text-slate-400 truncate pr-20">Episode: {film.episode_id}</h5>
+                        </div>
+                    </article>
                 </li>
             ))}
         </ul>
