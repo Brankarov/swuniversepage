202ff6da4ee58b99fe4777f1f9633cf6354d91a8
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-11 12:52:18 -0300

    add(-): add mod to characters

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -15,7 +15,9 @@ export default async function CharacterPage(){
                 <li key={char.id}>
                     <div className="max-w-xs bg-slate-900 rounded overflow-hidden shadow-lg m-4 items-center justify-center">
                         <h1 className="text-semibold items-center flex justify-center">{char.name}</h1>
-                        <Image src={char.img} height={200} width={100} className="bg-slate-800 items-center" />
+                        <Image src={char.img} height={150} width={150} className="bg-slate-800 "/>
+                        <h2 className="text-center felx justify-center">Eyes Color: {char.eye_color}</h2>
+                        <h2 className="text-center justify-center">Gender: {char.gender}</h2>
                     </div>
                 </li>
             ))}
