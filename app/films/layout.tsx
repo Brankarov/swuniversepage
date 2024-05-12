9ec0737e17956302025b96e275ab23a03cff1a14
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-12 11:37:04 -0300

    fix(-): added some tailwind css to show the correct side of movies

--- app/films/layout.tsx
+++ app/films/layout.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from "react";
 
 export default function Layout({children} : {children : ReactNode}){
     return(
-        <div>
-            <div>
+        <div className="flex h-screen">
+            <div className="w-1/2 bg-slate-800 p-4 overflow-y-auto">
                 Layaout here
             </div>
-            <div>
+            <div className="w-1/2 bg-slate-700 p-4 overflow-y-auto">
                 children here
                 {children}
             </div>
