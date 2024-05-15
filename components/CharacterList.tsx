9e07451fcbf58b741b51f614f7e1dbae4f7b70d1
Leonel Sebastian Gergoff <leo_seba_93@hotmail.com>
2024-05-15 10:11:04 -0300

    Add(-) : added  filter function in CharacterListComponent

--- components/CharacterList.tsx
+++ components/CharacterList.tsx
@@ -1,17 +1,47 @@
 import { GetAllCharacters } from "@/Data/funtions/FetchData/GetAllCharacters";
 import { Character } from "@/interfaces/characterInterface";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import CharacterItemList from "./CharacterItemList";
+
 
 export default async function CharacterList(){
     const chars : Character[] = await GetAllCharacters();
+    
+    useEffect(()=>{
+        setFilteredData(chars);
+    }, []);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setSearch(e.target.value)
+        if(search.length > 1){
+            const filteredValues = chars.filter( row =>{
+                const rawGender = row.gender?.toLocaleLowerCase();
+                const rawEyes = row.eye_color?.toLocaleLowerCase();
+                const q = e.target.value.toLowerCase();
+                if(rawGender?.includes(q) || rawEyes?.includes(q)){
+                    return true
+                }
+                return false
+            })
+            setFilteredData(filteredValues);
+        }
+    }
+    const [search, setSearch] = useState('');
+    const [filteredData, setFilteredData] = useState<Character[]>([]);
+    
+    
     return(
-        <ul className="bg-slate-900 flex flex-wrap">
-            <Suspense fallback={<div> Cargando characters</div>}>
-                {chars.map((char)=>(
-                   <CharacterItemList key={char.id} {...char}/>
-                ))}
-            </Suspense>
-        </ul>
+        <div>
+            <div className="flex justify-center align-items-center">
+                <input type="text" placeholder="Search" value={search} onChange={handleSearch} className="block rounded-md border-solid  border border-inherit py-2 px-4 mb5"/>
+            </div>
+            <ul className="bg-slate-900 flex flex-wrap">
+                <Suspense fallback={<div> Cargando characters</div>}>
+                    {filteredData.map((char)=>(
+                    <CharacterItemList key={char.id} {...char}/>
+                    ))}
+                </Suspense>
+            </ul>
+        </div>
     );
 };
